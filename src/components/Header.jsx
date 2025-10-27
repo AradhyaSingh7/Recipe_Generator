a5480b4ad50930ce150d9cf90c040af4bbcfa329
AradhyaSingh7 <aradhya07singh@gmail.com>
2025-10-27 19:22:31 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ export default function ClaudeRecipe(props) {
     return (
         <>
         <div className="heading">
-            <img src="public\images\chef.png" alt="chef-icon" className="chef-icon-header"></img>
+            <img src="images/chef.png" alt="chef-icon" className="chef-icon-header"></img>
             <h2>WhiskAI</h2>
         </div>
         <div className="heading-p">
